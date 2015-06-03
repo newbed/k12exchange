59231a16f2341ecc805e90198d3d377f0034f6ec
Dave Newberry <david.newberry@gmail.com>
2015-06-03 15:05:12 -0500

    generate and migrate ratings model

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,50 @@
+class RatingsController < ApplicationController
+  def index
+    @ratings = Rating.all
+  end
+
+  def show
+    @rating = Rating.find(params[:id])
+  end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    @rating = Rating.new
+    @rating.stars = params[:stars]
+    @rating.comment = params[:comment]
+
+    if @rating.save
+      redirect_to "/ratings", :notice => "Rating created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @rating = Rating.find(params[:id])
+  end
+
+  def update
+    @rating = Rating.find(params[:id])
+
+    @rating.stars = params[:stars]
+    @rating.comment = params[:comment]
+
+    if @rating.save
+      redirect_to "/ratings", :notice => "Rating updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @rating = Rating.find(params[:id])
+
+    @rating.destroy
+
+    redirect_to "/ratings", :notice => "Rating deleted."
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603195502) do
+ActiveRecord::Schema.define(version: 20150603200444) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "url"
     t.string   "title"
     t.float    "avg_rating"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "stars"
+    t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
